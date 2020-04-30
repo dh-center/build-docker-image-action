3d700b1da6bda0a3de2ff742955042413bc5b7a8
Nikita Melnikov <nikmel2803@gmail.com>
2020-04-30 10:36:30 +0300

    fix tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,14 +1,9 @@
-import * as process from 'process';
 import * as cp from 'child_process';
 import * as path from 'path';
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500';
-  const ip = path.join(__dirname, '..', 'lib', 'main.js');
-  const options: cp.ExecSyncOptions = {
-    env: process.env,
-  };
+  const ip = path.join(__dirname, '..', 'dist', 'index.js');
 
-  console.log(cp.execSync(`node ${ip}`, options).toString());
+  console.log(cp.execSync(`node ${ip}`).toString());
 });
