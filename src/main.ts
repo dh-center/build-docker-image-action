b4966b18db7447fad6edc2d52604fed42d2cd916
Nikita Melnikov <nikmel2803@gmail.com>
2020-04-30 10:14:25 +0300

    add react preset

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import * as core from '@actions/core';
 import { exec } from '@actions/exec';
+import { reactPreset } from './presets';
 
 /**
  * Action's main function
  */
 async function run(): Promise<void> {
   try {
-    console.log('It works!');
-    console.log(exec('docker ps'));
-    console.log(exec('pwd'));
-    console.log(exec('ls -la'));
+    await exec(`${reactPreset} docker build . -f-`);
   } catch (error) {
     core.setFailed(error.message);
   }
