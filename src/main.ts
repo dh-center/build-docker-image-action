b1a5143a8b03297cb1a01a9db71eb1612519c895
Nikita Melnikov <nikmel2803@gmail.com>
2020-04-30 18:52:44 +0300

    download preset from github

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import * as core from '@actions/core';
 import { exec } from '@actions/exec';
-import { reactPreset } from './presets';
+import axios from 'axios';
 
 /**
  * Action's main function
  */
 async function run(): Promise<void> {
-  try {
-    core.info('Start building docker image');
-    await exec('docker build -f - .', undefined, {
-      input: Buffer.from(reactPreset),
-    });
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  core.info('Start building docker image');
+
+  const dockerfileLink = 'https://raw.githubusercontent.com/dh-center/build-docker-image-action/master/presets/react-prod/Dockerfile';
+  const preset = (await axios.get(dockerfileLink)).data;
+
+  await exec('docker build -f - .', undefined, {
+    input: Buffer.from(preset),
+  });
 }
 
-run();
+run()
+  .catch(error => core.setFailed(error.message));
