bff9ef2b0459747f0b5a96f842b44b5c5b117ff9
Nikita Melnikov <nikmel2803@gmail.com>
2020-04-30 09:43:57 +0300

    test exec commands

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import * as core from '@actions/core';
+import { exec } from '@actions/exec';
 
 /**
  * Action's main function
  */
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds');
-
-    core.debug(`Waiting ${ms} milliseconds ...`);
+    console.log('It works!');
+    console.log(exec('docker ps'));
+    console.log(exec('pwd'));
+    console.log(exec('ls -la'));
   } catch (error) {
     core.setFailed(error.message);
   }
